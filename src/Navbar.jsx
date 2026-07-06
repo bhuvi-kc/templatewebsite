2e5623d6932e84ab819b08ab5646e7635bfc3461
Whatyadoinn <minehome205@gmail.com>
2026-07-06 20:56:00 +0530

    Describe what you changed

---
+++ src/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { useState } from "react";
+import { NavLink } from "react-router-dom";
+
+const Navbar=()=>{
+    return(<>
+    <h1>
+        
+    </h1>
+    </>)
+}
+
+export default Navbar
